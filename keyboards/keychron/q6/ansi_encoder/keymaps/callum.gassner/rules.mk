6fce53cc8f97ab7d809764e29209c32dc37d2923
callumgassner <94658141+callumgassner@users.noreply.github.com>
2024-07-27 13:38:02 +1000

    per layer debounce
    layers 0,1: sym_eager_pk
    layers 2,3: none

--- keyboards/keychron/q6/ansi_encoder/keymaps/callum.gassner/rules.mk
+++ keyboards/keychron/q6/ansi_encoder/keymaps/callum.gassner/rules.mk
@@ -1,8 +1,11 @@
 VIA_ENABLE = no
 ENCODER_MAP_ENABLE = yes
 
+# DEBOUNCE_TYPE = sym_eager_pk
+DEBOUNCE_TYPE = custom
 LTO_ENABLE = yes
 MOUSEKEY_ENABLE = no
 
 CONSOLE_ENABLE = no
 
+SRC += quantum/debounce/debounce.c
